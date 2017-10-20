f98839ee6c4536c2464056edb3cd6290e8e76a61
Stanislav Kyian <mancersoft@gmail.com>
2017-10-20 23:03:47 +0300

    Add bindings and interface improvements

--- YoloMark/MainWindow.xaml.cs
+++ YoloMark/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Diagnostics;
 
 namespace YoloMark
 {
@@ -24,19 +25,56 @@ namespace YoloMark
         private const int PreviewImagesCount = 5;
         private const int SelectBoxBorderWidth = 5;
         private const int SelectBoxObjectNameFontSize = 30;
+        private const int MinSelectBoxWidth = 15;
+        private const int MinSelectBoxHeight = 15;
+
+        private Dictionary<Key, int> keyObjectBinding = new Dictionary<Key, int>()
+        {
+            { Key.D3, 0 },
+            { Key.D4, 1 },
+            { Key.D5, 2 },
+            { Key.D6, 3 },
+            { Key.D8, 4 },
+            { Key.D9, 5 },
+            { Key.NumPad3, 0 },
+            { Key.NumPad4, 1 },
+            { Key.NumPad5, 2 },
+            { Key.NumPad6, 3 },
+            { Key.NumPad8, 4 },
+            { Key.NumPad9, 5 },
+            { Key.A, 6 },
+            { Key.B, 7 },
+            { Key.C, 8 },
+            { Key.D, 9 },
+            { Key.E, 10 },
+            { Key.H, 11 },
+            { Key.J, 12 },
+            { Key.K, 13 },
+            { Key.M, 14 },
+            { Key.N, 15 },
+            { Key.P, 16 },
+            { Key.R, 17 },
+            { Key.S, 18 },
+            { Key.T, 19 },
+            { Key.U, 20 },
+            { Key.V, 21 },
+            { Key.W, 22 },
+            { Key.X, 23 },
+            { Key.Y, 24 },
+        };
 
         private string ImgFolder = AppDomain.CurrentDomain.BaseDirectory + @"data\img\";
         private string TrainFilename = AppDomain.CurrentDomain.BaseDirectory + @"data\train.txt";
         private string NamesFilename = AppDomain.CurrentDomain.BaseDirectory + @"data\obj.names";
 
-        private Polygon selectBox;
+        private Rectangle selectBox;
         private Point selectBoxStartPos;
-        private TextBlock selectBoxObjectName;
+        private TextBlock selectBoxTextBlock;
         private Random random = new Random();
-        private int currentImageNumber = 0;
-        private int currentObjectNumber = 0;
         private Image[] previewImages = new Image[PreviewImagesCount];
         private Image[] previewImagesCheck = new Image[PreviewImagesCount];
+        private List<Rectangle> currentBoxes = new List<Rectangle>();
+        private List<TextBlock> currentTextBlocks = new List<TextBlock>();
 
         public MainWindow()
         {
@@ -63,15 +101,13 @@ namespace YoloMark
             }
             fout.Close();
 
+            SliderImageNumber.Maximum = 500;
+            SliderObjectNumber.Maximum = 24;
             MainCanvas.Background = new ImageBrush(new BitmapImage(new Uri(imgFiles[0])));
-
-
-        }
-
-        private void MainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            TextBoxWidth.Text = e.NewSize.Width.ToString();
-            TextBoxHeight.Text = e.NewSize.Height.ToString();
+            for (int i = 0; i < PreviewImagesCount; i++)
+            {
+                this.previewImages[i].Source = new BitmapImage(new Uri(imgFiles[i]));
+            }
         }
 
         private void ResizeButton_Click(object sender, RoutedEventArgs e)
@@ -82,50 +118,12 @@ namespace YoloMark
             MainWnd.Height = height;
         }
 
-        private void AddSelectedRect()
-        {
-            if (this.selectBox != null)
-            {
-                this.selectBox = null;
-                return;
-            }
-
-
-        }
-
-        private void MainCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.AddSelectedRect();
-            SolidColorBrush boxColor = new SolidColorBrush(Color.FromRgb((byte)(random.Next() % 256), (byte)(random.Next() % 256), (byte)(random.Next() % 256)));
-            this.selectBox = new Polygon()
-            {
-                StrokeThickness = SelectBoxBorderWidth,
-                Stroke = boxColor
-            };
-            this.selectBoxStartPos = Mouse.GetPosition(MainCanvas);
-            this.selectBox.Points.Add(new Point(this.selectBoxStartPos.X, this.selectBoxStartPos.Y));
-            this.selectBox.Points.Add(new Point(this.selectBoxStartPos.X, this.selectBoxStartPos.Y));
-            this.selectBox.Points.Add(new Point(this.selectBoxStartPos.X, this.selectBoxStartPos.Y));
-            this.selectBox.Points.Add(new Point(this.selectBoxStartPos.X, this.selectBoxStartPos.Y));
-            MainCanvas.Children.Add(this.selectBox);
-
-            this.selectBoxObjectName = new TextBlock()
-            {
-                Text = "test",
-                Foreground = boxColor,
-                FontSize = SelectBoxObjectNameFontSize
-            };
-            Canvas.SetLeft(this.selectBoxObjectName, this.selectBoxStartPos.X + SelectBoxBorderWidth);
-            Canvas.SetTop(this.selectBoxObjectName, this.selectBoxStartPos.Y + SelectBoxBorderWidth - 10);
-            MainCanvas.Children.Add(this.selectBoxObjectName);
-        }
-
-        private static Point GetLeftTopPoint(PointCollection points)
+        private static Point GetTopLeftPoint(PointCollection points)
         {
             Point res = new Point(int.MaxValue, int.MaxValue);
             foreach (Point point in points)
             {
-                if ((int)point.X < (int)res.X || ((int)point.X == (int)res.X && (int)point.Y < (int)res.Y))
+                if ((int)point.Y < (int)res.Y || ((int)point.Y == (int)res.Y && (int)point.X < (int)res.X))
                 {
                     res = point;
                 }
@@ -134,48 +132,184 @@ namespace YoloMark
             return res;
         }
 
-        private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
+        private static Point GetBottomRightPoint(PointCollection points)
+        {
+            Point res = new Point(int.MinValue, int.MinValue);
+            foreach (Point point in points)
+            {
+                if ((int)point.Y > (int)res.Y || ((int)point.Y == (int)res.Y && (int)point.X > (int)res.X))
+                {
+                    res = point;
+                }
+            }
+
+            return res;
+        }
+
+        private void MainWnd_MouseMove(object sender, MouseEventArgs e)
         {
             if (this.selectBox != null)
             {
                 Point mousePos = Mouse.GetPosition(MainCanvas);
-                if (mousePos.X < 0 || mousePos.Y < 0 || mousePos.X > MainCanvas.Width || mousePos.Y > MainCanvas.Height)
+                mousePos.X = mousePos.X < 0 ? 0 : mousePos.X;
+                mousePos.Y = mousePos.Y < 0 ? 0 : mousePos.Y;
+                mousePos.X = mousePos.X > MainCanvas.ActualWidth ? MainCanvas.ActualWidth : mousePos.X;
+                mousePos.Y = mousePos.Y > MainCanvas.ActualHeight ? MainCanvas.ActualHeight : mousePos.Y;
+
+                var pointCollection = new PointCollection()
                 {
-                    return;
-                }
+                    this.selectBoxStartPos,
+                    mousePos,
+                    new Point(this.selectBoxStartPos.X, mousePos.Y),
+                    new Point(mousePos.X, this.selectBoxStartPos.Y)
+                };
+                Point topLeftPoint = GetTopLeftPoint(pointCollection);
+                Point bottomRightPoint = GetBottomRightPoint(pointCollection);
+                Canvas.SetLeft(this.selectBox, topLeftPoint.X);
+                Canvas.SetTop(this.selectBox, topLeftPoint.Y);
+                this.selectBox.Width = bottomRightPoint.X - topLeftPoint.X;
+                this.selectBox.Height = bottomRightPoint.Y - topLeftPoint.Y;
 
-                this.selectBox.Points[1] = new Point(this.selectBox.Points[0].X, mousePos.Y);
-                this.selectBox.Points[2] = new Point(mousePos.X, mousePos.Y);
-                this.selectBox.Points[3] = new Point(mousePos.X, this.selectBox.Points[0].Y);
-
-                Point leftTopPoint = GetLeftTopPoint(this.selectBox.Points);
-                Canvas.SetLeft(this.selectBoxObjectName, leftTopPoint.X + SelectBoxBorderWidth);
-                Canvas.SetTop(this.selectBoxObjectName, leftTopPoint.Y + SelectBoxBorderWidth - 10);
+                Canvas.SetLeft(this.selectBoxTextBlock, topLeftPoint.X + SelectBoxBorderWidth);
+                Canvas.SetTop(this.selectBoxTextBlock, topLeftPoint.Y + SelectBoxBorderWidth - 10);
             }
         }
 
-        private void MainCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private bool AddSelectBox()
         {
-            this.AddSelectedRect();
+            if (this.selectBox != null)
+            {
+                if (this.selectBox.Width < MinSelectBoxWidth || this.selectBox.Height < MinSelectBoxHeight)
+                {
+                    this.RemoveSelectBox();
+                    return false;
+                }
+
+                this.currentBoxes.Add(this.selectBox);
+                this.currentTextBlocks.Add(this.selectBoxTextBlock);
+                this.selectBox = null;
+                return true;
+            }
+
+            return false;
         }
 
-        private void MainCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        private bool RemoveSelectBox()
         {
             if (this.selectBox != null)
             {
                 MainCanvas.Children.Remove(this.selectBox);
+                MainCanvas.Children.Remove(this.selectBoxTextBlock);
                 this.selectBox = null;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void RemoveCurrentBoxes()
+        {
+            for (int i = 0; i < this.currentBoxes.Count; i++)
+            {
+                MainCanvas.Children.Remove(this.currentBoxes[i]);
+                MainCanvas.Children.Remove(this.currentTextBlocks[i]);
+            }
+
+            this.currentBoxes.Clear();
+            this.currentTextBlocks.Clear();
+        }
+
+        private void MainWnd_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.AddSelectBox())
+            {
+                return;
+            }
+
+            Point mousePos = Mouse.GetPosition(MainCanvas);
+            if (mousePos.X < 0 || mousePos.Y < 0 || mousePos.X > MainCanvas.ActualWidth || mousePos.Y > MainCanvas.ActualHeight)
+            {
+                return;
+            }
+
+            SolidColorBrush boxColor = new SolidColorBrush(Color.FromRgb((byte)(random.Next() % 256), (byte)(random.Next() % 256), (byte)(random.Next() % 256)));
+            this.selectBox = new Rectangle()
+            {
+                StrokeThickness = SelectBoxBorderWidth,
+                Stroke = boxColor
+            };
+            this.selectBoxStartPos = mousePos;
+            Canvas.SetLeft(this.selectBox, this.selectBoxStartPos.X);
+            Canvas.SetTop(this.selectBox, this.selectBoxStartPos.Y);
+            this.selectBox.Width = 0;
+            this.selectBox.Height = 0;
+            MainCanvas.Children.Add(this.selectBox);
+
+            this.selectBoxTextBlock = new TextBlock()
+            {
+                Text = SliderObjectNumber.Value.ToString(),
+                Foreground = boxColor,
+                FontSize = SelectBoxObjectNameFontSize
+            };
+            Canvas.SetLeft(this.selectBoxTextBlock, this.selectBoxStartPos.X + SelectBoxBorderWidth);
+            Canvas.SetTop(this.selectBoxTextBlock, this.selectBoxStartPos.Y + SelectBoxBorderWidth - 10);
+            MainCanvas.Children.Add(this.selectBoxTextBlock);
+        }
+
+        private void MainWnd_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.AddSelectBox();
+        }
+
+        private void MainWnd_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RemoveSelectBox();
+        }
+
+        private void MainWnd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (this.keyObjectBinding.ContainsKey(e.Key))
+            {
+                SliderObjectNumber.Value = this.keyObjectBinding[e.Key];
+            }
+        }
+
+        private void MainWnd_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+
+                    this.RemoveCurrentBoxes();
+                    break;
+                case Key.Right:
+
+                    this.RemoveCurrentBoxes();
+                    break;
+                case Key.Back:
+                    if (this.currentBoxes.Count > 0)
+                    {
+                        MainCanvas.Children.Remove(this.currentBoxes.Last());
+                        MainCanvas.Children.Remove(this.currentTextBlocks.Last());
+                        this.currentBoxes.RemoveAt(this.currentBoxes.Count - 1);
+                        this.currentTextBlocks.RemoveAt(this.currentTextBlocks.Count - 1);
+                    }
+
+                    break;
+                case Key.Delete:
+
+                    this.RemoveCurrentBoxes();
+                    break;
             }
         }
 
         private void SliderImageNumber_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            this.currentImageNumber = (int)e.NewValue;
-        }
-
-        private void SliderObjectNumber_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            this.currentObjectNumber = (int)e.NewValue;
+            string[] imgFiles = Directory.GetFiles(ImgFolder, "*.jpg");
+            for (int i = 0; i < PreviewImagesCount; i++)
+            {
+                this.previewImages[i].Source = new BitmapImage(new Uri(imgFiles[(int)e.NewValue + i]));
+            }
         }
     }
 }
